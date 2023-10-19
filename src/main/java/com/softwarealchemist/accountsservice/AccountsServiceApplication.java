b6a6bc80db652bccd5f6a807998de96bc4f35d5e
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2023-10-19 14:49:20 -0600

    Swagger bby!

--- src/main/java/com/softwarealchemist/accountsservice/AccountsServiceApplication.java
+++ src/main/java/com/softwarealchemist/accountsservice/AccountsServiceApplication.java
@@ -1,11 +1,35 @@
 package com.softwarealchemist.accountsservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-//@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Account microservice REST API Documentation",
+                description = "Software-Alchemist Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Software Alchemist",
+                        email = "software@alchemist.com",
+                        url = "https://www.softare-alhcemist.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Software-Alchemist Accounts microservice REST API Documentation",
+                url = "https://www.software-alchemist.com"
+        )
+)
 public class AccountsServiceApplication {
 
     public static void main(String[] args) {
